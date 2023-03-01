5f5cf89db4a2a1d2450f123bb4be4d1866fd90b0
Lucas Valladon <valladonlucas@gmail.com>
2023-03-01 10:04:41 +0100

    gtts

--- audio/stt.py
+++ audio/stt.py
@@ -1,4 +1,6 @@
 import speech_recognition as sr
+import gtts
+from playsound import playsound
 
 r = sr.Recognizer()
 with sr.Microphone() as source:
@@ -6,6 +8,13 @@ with sr.Microphone() as source:
     audio = r.listen(source, timeout=2)
 
 try:
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+    print("Google Speech Recognition thinks you said " + r.recognize_google(audio, language="fr-FR"))
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
+
+# make request to google to get synthesis
+tts = gtts.gTTS(audio)
+# save the audio file
+tts.save("hello.mp3")
+# play the audio file
+playsound("hello.mp3")
