c561041cce0be267ef8f1bf3bb974fe3d63e53bf
Paul AUBRY <baekeland@outlook.fr>
2023-03-01 09:43:36 +0100

    audio ok

---
+++ audio/library.md
@@ -0,0 +1,6 @@
+# Library pour l'audio
+Speech Recognition est une librairie Python permettant de faire de la reconnaissance vocale. Elle est compatible avec Python 2.7 et 3.3+ et supporte plusieurs API de reconnaissance vocale, dont Google Speech Recognition, Microsoft Bing Voice Recognition et IBM Speech to Text.
+En particulier, on utilise l'API de Google Speech Recognition pour reconnaître la parole dans un fichier audio.
+```bash
+pip3 install speech_recognition
+```
--- audio/stt.py
+++ audio/stt.py
@@ -1,11 +1,15 @@
 import speech_recognition as sr
+from os import path
+
+AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "audio.wav")
 
 r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    audio = r.listen(source, timeout=2)
+with sr.AudioFile(AUDIO_FILE) as source:
+    audio = r.record(source) 
 
 try:
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+    print("Google Speech Recognition thinks you said " + r.recognize_google(audio,language="fr-FR"))
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
