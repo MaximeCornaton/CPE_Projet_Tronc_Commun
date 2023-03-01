df34efd3d79c555c45372532bd0d199de8269591
Lucas Valladon <valladonlucas@gmail.com>
2023-03-01 11:19:07 +0100

    ChatGPT répond vocalement

--- audio/stt.py
+++ audio/stt.py
@@ -7,9 +7,9 @@ from os import path
 AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "audio.wav")
 
 r = sr.Recognizer()
-with sr.AudioFile(AUDIO_FILE) as source:
-    audio = r.record(source)
-    text =  r.recognize_google(audio, language="fr-FR")
+with sr.Microphone() as source:
+    audio = r.listen(source,timeout=3)
+text =  r.recognize_google(audio, language="fr-FR")
 
 try:
     print("Google Speech Recognition thinks you said " + text)
