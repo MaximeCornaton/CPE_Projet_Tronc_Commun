29ec4983f9d580bd8b29ed19a4fba21878200e4b
maximecornaton <maxime.cornaton@cpe.fr>
2023-05-10 12:04:59 +0200

    oui

--- app/flutter_application_1/lib/pHome.dart
+++ app/flutter_application_1/lib/pHome.dart
@@ -22,6 +22,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Text("home");
+    return const Text("home");
   }
 }
